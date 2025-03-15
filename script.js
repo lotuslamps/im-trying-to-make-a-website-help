905707967684cc9a2561f828e8341a95b99465bc
lotuslamps <lotu.lamp36@gmail.com>
2025-03-15 11:16:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello, world!");
+function button() {
+  window.alert("sorry i deleted all your files :(")
+  var thing = document.getElementById("foobar")
+  thing.classList.add("javascript-bs")
+}
